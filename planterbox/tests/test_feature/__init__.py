f5906636246548070a5bfb8bfd7cefbc55fdd1d8
Nick Pilon <npilon@lexmachina.com>
2015-05-05 13:48:37 -0700

    Update step style.

--- planterbox/tests/test_feature/__init__.py
+++ planterbox/tests/test_feature/__init__.py
@@ -2,26 +2,26 @@ from planterbox import step
 
 
 @step(r'I add (\d+) and (\d+)')
-def add(self, a, b):
+def add(test, a, b):
     a = int(a)
     b = int(b)
-    self.result = a + b
+    test.result = a + b
 
 
 @step(r'the result should be (\d+)')
-def check_result(self, value):
+def check_result(test, value):
     value = int(value)
-    self.assertEqual(self.result, value)
+    test.assertEqual(test.result, value)
 
 
 @step(r'I add x = (?P<x>\d+) and y = (?P<y>\d+) -> (?P<result_name>\w+)')
-def add_keywords(self, y, x, result_name):
+def add_keywords(test, y, x, result_name):
     y = int(y)
     x = int(x)
-    setattr(self, result_name, x + y)
+    setattr(test, result_name, x + y)
 
 
 @step(r'I check (?P<result_name>\w+) == (?P<value>\d+)')
-def check_result_keyword(self, value, result_name):
+def check_result_keyword(test, value, result_name):
     value = int(value)
-    self.assertEqual(getattr(self, result_name), value)
+    test.assertEqual(getattr(test, result_name), value)
