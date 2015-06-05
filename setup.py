f7635870356f1079c3b8a83b2061eae824b98b4a
Nick Pilon <npilon@lexmachina.com>
2015-06-05 10:22:05 -0700

    Release 0.4.1

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
 description = 'A plugin for nose2 implementing behavior-driven testing.'
 
 setup(name='planterbox',
-      version='0.4',
+      version='0.4.1',
       description=description,
       license='MIT',
       classifiers=[
